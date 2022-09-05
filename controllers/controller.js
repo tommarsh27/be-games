b6d678753f4d7a70831e88e5d46f845b717856e2
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-05 14:30:00 +0100

    console log added to controller for reference

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,7 @@ const { selectCategories } = require('../models/model')
 
 exports.getCategories = (req, res, next) => {
     selectCategories().then((categories)=>{
+        // console.log({categories})
         res.status(200).send({categories})
     })
     .catch(next)
