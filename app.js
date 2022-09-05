05d67ddc078e451eb91e673b8674d0af999d6168
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-05 13:01:36 +0100

    app updated

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const { getCategories } = require('./controllers/controller')
 
 
 const app = express()
 
 app.use(express.json())
 
-
+app.get('/api/categories', getCategories)
 
 
 
