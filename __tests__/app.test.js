0bf9be11518585e98bdf0e84e81c74c5e1603412
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-05 12:39:35 +0100

    happy path test for GET/categories added

---
+++ __tests__/app.test.js
@@ -0,0 +1,32 @@
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data/index');
+const db = require('../db/connection');
+const app = require('../app');
+const request = require('supertest');
+const { response } = require('../app');
+
+beforeEach(() => {
+    return seed(testData)
+});
+
+afterAll(() => {
+   return db.end()
+})
+
+describe('/api/categories', () => {
+    describe('GET', () => {
+        test('200: responds with an object containing a key of categories and a value of an array containing all category objects', () => {
+            return request(app)
+            .get('api/categories')
+            .expect(200)
+            .then(({body}) => {
+                expect(Array.isArray(body.categories)).toBe(true)
+                expect(body.categories.length > 0)
+                body.categories.forEach((category) => {
+                    expect(category).toHaveProperty('slug', expect.any(String))
+                    expect(category).toHaveProperty('description', expect.any(String))
+                })
+            })
+        })
+    })
+})
