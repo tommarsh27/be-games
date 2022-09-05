71ccf005df949ceb7e5cbb9ab7b9f265a9437b8c
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-05 13:03:33 +0100

    model updated

--- models/model.js
+++ models/model.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection')
 
 exports.selectCategories = () => {
-    return db.query(queryStr, queryValues)
+    return db.query(`SELECT * FROM categories`)
     .then(({rows})=>{
         return rows
     })
