267f925c03e3aff7f00425cf9d04d2c2d0b34646
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-06 15:56:14 +0100

    review_id not found handled

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ app.get('/api/categories', getCategories)
 app.get('/api/reviews/:review_id', getReveiws)
 
 app.use((err, req, res, next) => {
-    if(err.code === '22P02') {
-        res.status(400).send({msg: 'Bad Request'})
-    }
-})
-
-app.use((err, req, res, next) => {
     if(err.status && err.message) {
         res.status(err.status).send({msg: err.message})
     } else {
         next(err)
+    }
+})
+
+app.use((err, req, res, next) => {
+    if(err.code === '22P02') {
+        res.status(400).send({msg: 'Bad Request'})
     }
 })
 
--- models/reviews_model.js
+++ models/reviews_model.js
@@ -1,11 +1,16 @@
 const db = require('../db/connection')
 
 exports.selectReviews = (review_id) => {
-    // console.log(review_id, '< id in model')
     return db.query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id])
     .then(({rows})=>{
+        if(rows.length === 0) {
+            return Promise.reject({
+                status: 404,
+                message: 'Not Found'
+            })
+        } else {
         rows.forEach(row => row.created_at = row.created_at.toString())
-        // console.log(rows[0].created_at, '< rows - model')
         return rows
+        }
     })
 }
