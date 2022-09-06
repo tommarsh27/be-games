3dec18b1c20251a305af6a45655170409eb8d2ef
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-09-06 15:30:10 +0100

    filepath reordering deletions missed from previous commit

--- controllers/controller.js
+++
@@ -1,9 +0,0 @@
-const {request} = require('../app')
-const { selectCategories } = require('../models/model')
-
-exports.getCategories = (req, res, next) => {
-    selectCategories().then((categories)=>{
-        res.status(200).send({categories})
-    })
-    .catch(next)
-}
--- models/model.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db/connection')
-
-exports.selectCategories = () => {
-    return db.query(`SELECT * FROM categories`)
-    .then(({rows})=>{
-        return rows
-    })
-}
